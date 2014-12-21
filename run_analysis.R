54fe1f5f708d9fe619d5084c7d255b891dc7e108
Elizabeth Hines <elizabeth.hines623@gmail.com>
2014-12-21 17:18:15 -0500

    Adding tidy

--- run_analysis.R
+++ run_analysis.R
@@ -28,24 +28,31 @@ feature_labels <-read.table("./data/features.txt")
 feature_names<-make.names(feature_labels[,2], unique = TRUE)
 colnames(X)<- feature_names
 
-
+#only select the mean and std values (excluding meanFreq)
 X_sub <- select(X,contains("mean", ignore.case = FALSE), contains("std"), -contains("meanFreq")
 )
 
-rm(X_test,Y_test,subject_test,X_train,Y_train,subject_train)
+# do some cleanup
+rm(X_test,Y_test,subject_test,X_train,Y_train,subject_train, X)
 
+#give some labels to what will be the first 2 columns
 colnames(Y) <- "Activity"
 colnames(subject) <- "Subject"
 
+#add the subject and activity colums to the table
 all_measurements <- cbind(subject, Y, X_sub)
 
+#sub activity numbers with the descriptive names
 all_measurements$Activity <- factor(all_measurements$Activity,
                                    activity_labels$Number,
                                    activity_labels$Name)
 
+#group the measurements by subject and activity (30 subjects x 6 activities)
 grouped <- group_by(all_measurements, Subject, Activity)
 
+
+#take the average of all the measurement columns
 final_table <- summarise_each(grouped, funs(mean))
 
-
+#output the final table
 write.table(final_table, file = "tidy_data.txt",row.names = FALSE)
