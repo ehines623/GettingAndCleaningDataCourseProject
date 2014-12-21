60a7d31ff64a12eb6fc97144020d536f2a6e10f9
Elizabeth Hines <elizabeth.hines623@gmail.com>
2014-12-21 17:04:22 -0500

    Adding all the data

--- run_analysis.R
+++ run_analysis.R
@@ -3,6 +3,8 @@
 ## Data course
 library(dplyr)
 
+
+#read in all of the data
 X_test <- read.table("./data/test/X_test.txt")
 Y_test <- read.table("./data/test/y_test.txt")
 subject_test <- read.table("./data/test/subject_test.txt")
@@ -12,17 +14,18 @@ Y_train <- read.table("./data/train/y_train.txt")
 subject_train <- read.table("./data/train/subject_train.txt")
 
 
+#join the test and training samples
 X <- rbind(X_test, X_train)
 Y <- rbind(Y_test, Y_train)
 subject <- rbind(subject_test, subject_train)
 
+
+#get the useful labels for the data frame from the text files
 activity_labels <- read.table("./data/activity_labels.txt")
 colnames(activity_labels) <- c("Number","Name")
 
 feature_labels <-read.table("./data/features.txt")
-
 feature_names<-make.names(feature_labels[,2], unique = TRUE)
-
 colnames(X)<- feature_names
 
 
